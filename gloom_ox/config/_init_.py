2c7f93b1acb29cb37655153cbc92493d90a1ae53
NDTVC-AC.KE <roygichira084@gmail.com>
2026-04-07 01:07:01 +0300

    Add GLOOM-OX configuration initialization file

---
+++ gloom_ox/config/_init_.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+GLOOM-OX Configuration
+"""
+
+from gloom_ox.config.settings import VERSION, AUTHOR, REPO_URL, SYSTEM, IS_TERMUX
+
+__all__ = [
+    'VERSION',
+    'AUTHOR',
+    'REPO_URL',
+    'SYSTEM',
+    'IS_TERMUX'
+]
